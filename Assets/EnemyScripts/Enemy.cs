3d47de8a480d0eee8a9a7faf7a1381d0d783a570
Artur Swoboda (artuswo443) <artuswo443@student.polsl.pl>
2018-04-01 16:05:58 +0200

    Enemies with waves

--- Assets/EnemyScripts/Enemy.cs
+++ Assets/EnemyScripts/Enemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
-	public float speed = 10;
+	public float speed = 50;
 
 	private Transform target;
 	private int waypointIndex = 0;
---
+++ Assets/EnemyScripts/WaveSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour {
+
+	public Transform enemyPrefab;
+	public Transform spawnPoint;
+
+	public float timeBetweenWaves = 5;
+	private float countdown = 1;
+	private int waveIndex = 0;
+	public Text waveCountdownText; 
+
+	void Update()
+	{
+		if (countdown <= 0)
+		{
+			StartCoroutine (SpawnWave ());
+			countdown = timeBetweenWaves;
+		}
+		countdown -= Time.deltaTime;
+		waveCountdownText.text = Mathf.Floor(countdown).ToString();
+	}
+
+	IEnumerator SpawnWave () 
+	{
+		waveIndex++;
+		for (int i = 0; i < waveIndex; i++) {
+			SpawnEnemy();
+			yield return new WaitForSeconds(0.5f);
+		}
+
+
+	}
+
+	void SpawnEnemy(){
+		Instantiate (enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+	}
+}
