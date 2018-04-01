79693a206860f3be254b7990320a3f6ff7b309fb
Artur Swoboda (artuswo443) <artuswo443@student.polsl.pl>
2018-04-01 10:55:08 +0200

    First Enemy

---
+++ Assets/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public float speed = 10;
+
+	private Transform target;
+	private int waypointIndex = 0;
+
+	void Start()
+	{
+		target = Waypoints.points [0];
+	}
+
+	void Update()
+	{
+		Vector3 direction = target.position - transform.position;
+		transform.Translate (direction.normalized * speed * Time.deltaTime, Space.World);
+
+		if (Vector3.Distance (transform.position, target.position) <= 0.8)
+		{
+			GetNextWaypoint ();
+		}
+	}
+
+	private void GetNextWaypoint()
+	{
+		if (waypointIndex >= Waypoints.points.Length - 1) {
+			Destroy (gameObject);
+			return;
+		}
+
+		waypointIndex++;
+		target = Waypoints.points [waypointIndex];
+	}
+}
