880c370081ee32fa4280c9fa18990bcc9dd2240c
rzmisial <rzmisial@wp.pl>
2018-04-02 16:39:21 +0200

    Camera(basic) + first textures
    Added camera movement. For now camera movement is not clamped. Camera
    controlled with WSAD and mouse scroll wheel. Also, included first
    textures.

---
+++ Assets/CameraFocusController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFocusController : MonoBehaviour {
+
+    public float movementRatio;
+
+    void LateUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position -= new Vector3(movementRatio, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(movementRatio, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= new Vector3(0, 0, movementRatio);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(0, 0, movementRatio);
+        }
+    }
+}
---
+++ Assets/GeneralScripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject focusObject;
+
+    public float scrollRatio;
+
+    private float distance;
+
+    void Start()
+    {
+        distance = 40.0f;
+    }
+
+    void LateUpdate()
+    {
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0) // back
+        {
+            distance += scrollRatio;
+
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") > 0) // forward
+        {
+            distance -= scrollRatio;
+
+            if (distance < -100.0f)
+                distance = -100.0f;
+        }
+
+        transform.position = transform.rotation * new Vector3(0, 0, -distance) + focusObject.transform.position; ;
+    }
+}
