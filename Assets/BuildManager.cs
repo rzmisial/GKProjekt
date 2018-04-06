ea230df6a0b79e65d7c62b1040785e91789b9423
Tiziton <Tiziton@gmail.com>
2018-04-06 21:44:47 +0200

    Set places to build turrets and initialize first turret in shop.

---
+++ Assets/BuildManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    public GameObject standardTurret;
+
+    public static BuildManager Instance;
+
+    private GameObject turretToBuild;
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            return;
+        }
+
+        Instance = this;    
+    }
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+
+    private void Start()
+    {
+        turretToBuild = null;
+    }
+
+    public void SetTurretToBuild (GameObject turret)
+    {
+        turretToBuild = turret;
+    }
+}
---
+++ Assets/Shop.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour {
+
+
+    BuildManager buildManager;
+
+    private void Start()
+    {
+        buildManager = BuildManager.Instance;
+    }
+
+    public void PurchaseTurret()
+    {
+        buildManager.SetTurretToBuild(buildManager.standardTurret);
+    }
+}
