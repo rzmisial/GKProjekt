ab0a5e0dee836aefede317cf629bef14341fe890
Tiziton <Tiziton@gmail.com>
2018-05-04 01:16:33 +0200

    Added pause game

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject PauseUI;
+
+	void Update () {
+	    if (Input.GetKeyDown((KeyCode.Escape)) || Input.GetKeyDown(KeyCode.P))
+	    {
+	        TogglePause();
+	    }
+	}
+
+    public void TogglePause()
+    {
+        PauseUI.SetActive(!PauseUI.activeSelf);
+
+        if (PauseUI.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void RetryGame()
+    {
+        TogglePause();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Menu()
+    {
+
+    }
+}
